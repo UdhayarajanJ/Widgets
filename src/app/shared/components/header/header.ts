3b4ce91b5a7ac6646f0ad677338681d4865bd50e
Udhayarajan J <udhayarajanj@outlook.com>
2026-05-29 22:38:58 +0530

    add: header styles

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { faSolidPlus, faSolidGear } from '@ng-icons/font-awesome/solid';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss',
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ faSolidPlus, faSolidGear })]
 })
 export class Header {
 
