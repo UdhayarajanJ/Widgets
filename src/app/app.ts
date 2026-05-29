d0b5b6db3043764f5b537e4c1f0b058d55d42c2c
Udhayarajan J <udhayarajanj@outlook.com>
2026-05-29 22:18:16 +0530

    add: basic component creation and implementation of the widgets poc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { Widgets } from "./features/widgets/widgets";
+import { Header } from "./shared/components/header/header";
+import { Footer } from "./shared/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Widgets],
+  imports: [Widgets, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('widget-poc');
+  
 }
--- src/app/features/widgets/widgets.ts
+++ src/app/features/widgets/widgets.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-widgets',
   imports: [],
   templateUrl: './widgets.html',
   styleUrl: './widgets.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Widgets {
+
+export class Widgets implements OnInit {
+
+  constructor(private titleService: Title) { }
+
+  public ngOnInit(): void {
+    this.titleService.setTitle('Widgets');
+  }
 
 }
